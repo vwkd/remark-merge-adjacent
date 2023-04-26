4b462e918d3ead6662935c682105d265f8c6b56e
vwkd <33468089+vwkd@users.noreply.github.com>
2023-04-26 15:49:09 +0400

    main: add more tests

--- src/main_test.ts
+++ src/main_test.ts
@@ -29,3 +29,56 @@ Deno.test("two strong, single child", async () => {
 
   assertEquals(actual, expected);
 });
+
+Deno.test("two strong, multiple children", async () => {
+  const input = "<strong>foo<em>bar</em></strong><strong>baz</strong>";
+  const expected = "**foo_bar_baz**\n";
+
+  const actual = (await pipeline
+    .process(input)).toString();
+
+  assertEquals(actual, expected);
+});
+
+Deno.test("three strong, single child", async () => {
+  const input = "<strong>foo</strong><strong>bar</strong><strong>baz</strong>";
+  const expected = "**foobarbaz**\n";
+
+  const actual = (await pipeline
+    .process(input)).toString();
+
+  assertEquals(actual, expected);
+});
+
+Deno.test("two links, empty properties, single child", async () => {
+  const input = "<a>foo</a><a>bar</a>";
+  const expected = "[foobar]()\n";
+
+  const actual = (await pipeline
+    .process(input)).toString();
+
+  assertEquals(actual, expected);
+});
+
+Deno.test("two links, same properties, single child", async () => {
+  const input =
+    '<a href="https://example.com/foo">foo</a><a href="https://example.com/foo">bar</strong>';
+  const expected = "[foobar](https://example.com/foo)\n";
+
+  const actual = (await pipeline
+    .process(input)).toString();
+
+  assertEquals(actual, expected);
+});
+
+Deno.test("two links, different properties, single child", async () => {
+  const input =
+    '<a href="https://example.com/foo">foo</a><a href="https://example.com/bar">bar</strong>';
+  const expected =
+    "[foo](https://example.com/foo)[bar](https://example.com/bar)\n";
+
+  const actual = (await pipeline
+    .process(input)).toString();
+
+  assertEquals(actual, expected);
+});
